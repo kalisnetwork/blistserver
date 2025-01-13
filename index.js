471d53a4701db8027857951e15e2b02bbbcfd7c8
kalisnetwork <kalyankodisha@gmail.com>
2025-01-13 18:00:43 +0530

    Port Updated

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import cors from 'cors';
 import placesRoutes from './routes/placesRoutes.js';
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4444;
 
 app.use(cors());
 app.use(express.json());
